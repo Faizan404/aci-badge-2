1139a1b0e1300f9686040cbed7e0a9e60dd4a4fa
Faizan Mustafa <77750924+Faizan404@users.noreply.github.com>
2024-08-15 17:32:39 +0500

    day 1
    variables

---
+++ Learning Js/index.js
@@ -0,0 +1,243 @@
+// What is JavaScript?
+// Highlevel language
+// Multi paradigm
+// Single Threaded
+// Dynamic Programming Language
+// Multipurpose
+
+// sequence
+// decision/ selection
+// loop
+
+// let x = 5;
+// console.log(x);
+
+// function number(x){
+//     return x;
+// }
+// number(5);
+
+// class Number {
+//     number(x){
+//         return x;
+//     }
+// }
+// y = new Number();
+// y.number();
+
+// int x = 5;
+
+// let name = 23;
+// console.log(name);
+// alert("ALERT");
+// console.log("Hello World;");
+
+// console.log(2 > 5);
+// alert("Hello World");
+// document.write("<h1>Hello World</h1>");
+
+// UI/UX
+// Frontend
+// Backend
+// Database
+
+[
+  {
+    name: "John Doe",
+    age: 30,
+    isEmployed: true,
+    address: {
+      street: "123 Main St",
+      city: "Anytown",
+      state: "NY",
+    },
+    phoneNumbers: ["123-456-7890", "987-654-3210"],
+  },
+  {
+    name: "John Doe",
+    age: 30,
+    isEmployed: true,
+    address: {
+      street: "123 Main St",
+      city: "Anytown",
+      state: "NY",
+    },
+    phoneNumbers: ["123-456-7890", "987-654-3210"],
+  },
+  {
+    name: "John Doe",
+    age: 30,
+    isEmployed: true,
+    address: {
+      street: "123 Main St",
+      city: "Anytown",
+      state: "NY",
+    },
+    phoneNumbers: ["123-456-7890", "987-654-3210"],
+  },
+  {
+    name: "John Doe",
+    age: 30,
+    isEmployed: true,
+    address: {
+      street: "123 Main St",
+      city: "Anytown",
+      state: "NY",
+    },
+    phoneNumbers: ["123-456-7890", "987-654-3210"],
+  },
+  {
+    name: "John Doe",
+    age: 30,
+    isEmployed: true,
+    address: {
+      street: "123 Main St",
+      city: "Anytown",
+      state: "NY",
+    },
+    phoneNumbers: ["123-456-7890", "987-654-3210"],
+  },
+  {
+    name: "John Doe",
+    age: 30,
+    isEmployed: true,
+    address: {
+      street: "123 Main St",
+      city: "Anytown",
+      state: "NY",
+    },
+    phoneNumbers: ["123-456-7890", "987-654-3210"],
+  },
+  {
+    name: "John Doe",
+    age: 30,
+    isEmployed: true,
+    address: {
+      street: "123 Main St",
+      city: "Anytown",
+      state: "NY",
+    },
+    phoneNumbers: ["123-456-7890", "987-654-3210"],
+  },
+  {
+    name: "John Doe",
+    age: 30,
+    isEmployed: true,
+    address: {
+      street: "123 Main St",
+      city: "Anytown",
+      state: "NY",
+    },
+    phoneNumbers: ["123-456-7890", "987-654-3210"],
+  },
+  {
+    name: "John Doe",
+    age: 30,
+    isEmployed: true,
+    address: {
+      street: "123 Main St",
+      city: "Anytown",
+      state: "NY",
+    },
+    phoneNumbers: ["123-456-7890", "987-654-3210"],
+  },
+  {
+    name: "John Doe",
+    age: 30,
+    isEmployed: true,
+    address: {
+      street: "123 Main St",
+      city: "Anytown",
+      state: "NY",
+    },
+    phoneNumbers: ["123-456-7890", "987-654-3210"],
+  },
+  {
+    name: "John Doe",
+    age: 30,
+    isEmployed: true,
+    address: {
+      street: "123 Main St",
+      city: "Anytown",
+      state: "NY",
+    },
+    phoneNumbers: ["123-456-7890", "987-654-3210"],
+  },
+  {
+    name: "John Doe",
+    age: 30,
+    isEmployed: true,
+    address: {
+      street: "123 Main St",
+      city: "Anytown",
+      state: "NY",
+    },
+    phoneNumbers: ["123-456-7890", "987-654-3210"],
+  },
+];
+
+// Data Types
+// String         "faizan994181", 'faizan'
+// Integer        +1,2,3 | -1,-2,-3, 23.3455
+// Boolean          true / false
+
+// Array          [1,2,3,4,5]
+// Objects        {name: "faizan"}
+
+// what are variables
+
+// let, const, var
+// variable name          muhammadFaizanMustafa
+// =
+// value
+
+let age = 22;
+const name = "faizan";
+console.log(typeof age);
+// console.log(age);
+age = 34;
+// console.log(age);
+// name = "ali";
+// console.log(name);
+
+// =
+// ==     2 == 2
+// ===    2 === 2
+
+// +,-,/,*,%
+
+//? 1. Find the area of a rectangle where the length is 5 and the width is 8.
+
+const length = 5;
+const width = 8;
+
+const areaRec = length * width;
+console.log("Area of a rectangle is ", areaRec, "sq");
+
+//? 2. Find the area of a triangle where the base is 4 and the height is 3.
+
+const base = 4;
+const height = 3;
+
+const areaTri = base * height * (1 / 2);
+// const areaTri = base * height * 0.5;
+console.log("Area of traiangle is", areaTri, "sq");
+
+//? 3. Find the area of a circle where the radius is 3.
+const radius = 3;
+const PI = 3.14;
+const areaCircle = PI * radius ** 2;
+console.log(areaCircle);
+
+//? 4. Convert temperatures from Celsius to Fahrenheit and Fahrenheit to Celsius
+let celsius = 46;
+let toFahrenheit = (9 / 5) * celsius + 32;
+// console.log(toFahrenheit, "F");
+document.write(toFahrenheit, "F");
+
+let fahrenheit = 46;
+let toCelsius = fahrenheit - 32 * (5 / 9);
+// console.log(toCelsius, "C");
+alert(toCelsius, "C");
+
+
