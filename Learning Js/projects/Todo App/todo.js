7e9e8c4eee544ad67a59bc10f0df34abaa35c57c
Faizan Mustafa <77750924+Faizan404@users.noreply.github.com>
2024-08-30 17:40:31 +0500

    Todo App

--- Learning Js/projects/Todo App/todo.js
+++ Learning Js/projects/Todo App/todo.js
@@ -1,11 +1,8 @@
-// CREATE
-// READ
-// DELETE
-
-// let todoList = [];
+let todoList = [];
+const listGroup = document.getElementById("list-group");
+let index = 1;
 function createTodo() {
   const formCreate = document.querySelector(".form-create");
-  const listGroup = document.getElementById("list-group");
 
   formCreate.addEventListener("submit", function (e) {
     e.preventDefault();
@@ -14,24 +11,53 @@ function createTodo() {
     // guard clause
     if (!inputData) return alert("Please fill that field");
     const html = `
-        <li
-          class="list-group-item d-flex justify-content-between align-items-center"
-        >
-          <span>${inputData}</span>
-          <i class="far fa-trash-alt delete"></i>
-        </li>
+    <li
+    class="list-group-item d-flex justify-content-between align-items-center"
+    >
+    <span>${inputData}</span>
+    <ion-icon data-index=${index} name="close-outline" class="delete-icon"></ion-icon>
+    </li>
     `;
     listGroup.insertAdjacentHTML("afterbegin", html);
-    // readTodo(inputData);
+    todoList.push(inputData);
+    localStorage.setItem("todo", JSON.stringify(todoList));
     todoInput.value = "";
+    index++;
   });
 }
-// function readTodo(todo) {
-//   todoList.push(todo);
-  
-// }
-// function deleteTodo() {}
+function readTodo() {
+  let todoTemplate = "";
+  const todoItems = localStorage.getItem("todo");
+  if (!todoItems) return;
+  const todoArray = JSON.parse(todoItems);
+  todoList = todoArray;
+  todoTemplate = todoArray.map((todo, i) => {
+    return `<li
+    class="list-group-item d-flex justify-content-between align-items-center"
+    >
+    <span>${todo}</span>
+    <ion-icon data-index=${
+      i + 1
+    } name="close-outline" class="delete-icon"></ion-icon>
+    </li>
+    `;
+  });
+  listGroup.insertAdjacentHTML("afterbegin", todoTemplate.join(""));
+}
+function deleteTodo() {
+  listGroup.addEventListener("click", function (e) {
+    const deleteBtn = e.target.closest(".delete-icon");
+    if (!deleteBtn) return;
+    const todoIndex = deleteBtn.dataset.index;
+    todoList.splice(todoIndex - 1, 1);
+    localStorage.setItem("todo", JSON.stringify(todoList));
+    listGroup.innerHTML = "";
+    readTodo();
+  });
+}
 
 (function init() {
   createTodo();
+  readTodo();
+  deleteTodo();
 })();
